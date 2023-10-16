025876c92a119129343dadca05d3ed6a94cc51e8
Saxhaa <120165044+Saxhaa@users.noreply.github.com>
2023-10-16 16:11:52 +0200

    Update Utilisateur.java
    ajout des getters

--- Dév Log/Utilisateur.java
+++ Dév Log/Utilisateur.java
@@ -1,15 +1,31 @@
-import java.util.*;
+//import java.util.*;
 
 public class Utilisateur {
     private String nom;
-    private String prénom;
-    private double téléphone;
+    private String prenom;
+    private double telephone;
     private String mail;
 
-    public Utilisateur (String nom, String prénom, double téléphone, String mail){
+    public Utilisateur (String nom, String prenom, double telephone, String mail){
         this.nom = nom;
-        this.prénom = prénom;
-        this.téléphone = téléphone;
+        this.prenom = prenom;
+        this.telephone = telephone;
         this.mail = mail;
+    }
+    
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getPrenom() {
+        return this.prenom;
+    }
+
+    public double getTelephone() {
+        return this.telephone;
+    }
+
+    public String getMail() {
+        return this.mail;
     }
 }
