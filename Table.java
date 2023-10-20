d71e43a5ad307cd32f4f5fb1a0c9a7fea0028431
eoleds <120165071+eoleds@users.noreply.github.com>
2023-10-20 14:13:00 +0200

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Table {
+    private List<Utilisateur> utilisateurs;
+    private List<Demande> demandes;
+    private List<Proposition> propositions;
+
+    public Table() {
+        utilisateurs = new ArrayList<>();
+        demandes = new ArrayList<>();
+        propositions = new ArrayList<>();
+    }
+
+    public void ajouterUtilisateur (Utilisateur utilisateur) {
+        utilisateurs.add(utilisateur);
+    }
+
+    public void soumettreDemande (Demande demande){
+        demandes.add(demande);
+    }
+
+    public void soumettreProposition (Proposition proposition){
+        propositions.add(proposition);
+    }
+
+    public List<Demande> getDemandes(){
+        return demandes;
+    }
+
+    public List<Proposition> getPropositions(){
+        return propositions;
+    }
+}
